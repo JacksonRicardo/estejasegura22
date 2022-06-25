80aba65c28551ae61ae043e96a7316e5e28dcc91
Pedro Barros <pedro.hcb@Live.com>
2022-06-25 19:37:19 -0300

    Exclusão de push incorreto

--- backend/Projeto-api-todo/api_todo/app/serializers.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import field
-from app.models import Todo
-
-from rest_framework import serializers
-
-class TodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Todo
-        fields = ['id', 'name', 'done', 'created_at']
